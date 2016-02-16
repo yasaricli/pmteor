79f013607d35604386e6fa6690f2ce96ba8e9ff6
Yasar icli <hello@yasaricli.com>
2016-02-16 08:18:08 +0200

    #15 install method.

--- collections/bundles.js
+++ collections/bundles.js
@@ -30,62 +30,12 @@ isServer(() => {
 
       // extract ended callback.
       extract.stdout.on('end', Meteor.bindEnvironment(() => {
-        const application = Applications.findOne({
-          bundleId: file._id
-        });
 
         // REMOVE BUNDLE
         Bundles.remove(file._id);
 
-        // CD SERVER PACKAGES
-        shell.cd(`${file._id}/programs/server`);
-
-        // NPM PACKAGES INSTALL
-        const install = shell.exec('npm install', EXEC_OPTIONS);
-
-        /* || bindEnvironment
-         *
-         * if npm install completed then update
-         * application status READY..
-         */
-        install.stdout.on('end', Meteor.bindEnvironment(() => {
-
-          // npm-bcrypt FIX
-          if (shell.test('-e', 'npm/npm-bcrypt')) {
-            const bcrypt = shell.exec('npm install bcrypt', EXEC_OPTIONS);
-
-            // bcrypt end then
-            return bcrypt.stdout.on('end', Meteor.bindEnvironment(() => {
-
-              // REMOVE bcrypt DIR
-              shell.rm('-rf', 'npm/npm-bcrypt');
-
-
-              // cfs_gridfs FIX
-              if (shell.test('-e', 'npm/cfs_gridfs')) {
-
-                // CD BSON MODULE
-                shell.cd('npm/cfs_gridfs/node_modules/mongodb/node_modules/bson');
-
-                // MAKE COMMAND
-                const make = shell.exec('make', EXEC_OPTIONS);
-
-                // MAKE END THEN
-                return make.stdout.on('end', Meteor.bindEnvironment(() => {
-
-                  // UPDATE STATUS
-                  application.setStatus(3);
-                }));
-              }
-
-              // UPDATE STATUS
-              application.setStatus(3);
-            }));
-          }
-
-          // UPDATE STATUS
-          application.setStatus(3);
-        }));
+        // NPM INSTALL
+        Meteor.call('install', file._id); // file._id == BundleId
       }));
     }
   }));
--- server/methods.js
+++ server/methods.js
@@ -23,5 +23,64 @@ Meteor.methods({
         });
       }
     });
+  },
+
+  install(bundleId) {
+    const application = Applications.findOne({ bundleId });
+
+    // UPDATE STATUS PROGRESS
+    application.setStatus(1);
+
+    // CD SERVER PACKAGES
+    shell.cd(`${application.dir()}/programs/server`);
+
+    // NPM PACKAGES INSTALL
+    const install = shell.exec('npm install', EXEC_OPTIONS);
+
+    install.stdout.on('end', Meteor.bindEnvironment(() => {
+
+      // INSTALL COMPLETED UPDATE STATUS
+      application.setStatus(3);
+
+      // FIX PACKAGES
+      if (shell.test('-e', 'npm/npm-bcrypt')) {
+
+        // PROGRESS
+        application.setStatus(1);
+
+        // NPM BCRYPT INSTALL
+        const bcrypt = shell.exec('npm install bcrypt', EXEC_OPTIONS);
+
+        // bcrypt end then
+        return bcrypt.stdout.on('end', Meteor.bindEnvironment(() => {
+
+          // READY
+          application.setStatus(3);
+
+          // REMOVE bcrypt DIR
+          shell.rm('-rf', 'npm/npm-bcrypt');
+
+          // cfs_gridfs FIX
+          if (shell.test('-e', 'npm/cfs_gridfs')) {
+
+            // PROGRESS
+            application.setStatus(1);
+
+            // CD BSON MODULE
+            shell.cd('npm/cfs_gridfs/node_modules/mongodb/node_modules/bson');
+
+            // MAKE COMMAND
+            const make = shell.exec('make', EXEC_OPTIONS);
+
+            // MAKE END THEN
+            return make.stdout.on('end', Meteor.bindEnvironment(() => {
+
+              // PROGRESS
+              application.setStatus(3);
+            }));
+          }
+        }));
+      }
+    }));
   }
 });
