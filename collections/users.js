590d2893b64000832d7551c787c1caa028196e8b
Yasar icli <hello@yasaricli.com>
2016-02-22 13:23:37 +0200

    #30 Gravatar header user.

--- collections/users.js
+++ collections/users.js
@@ -1,1 +1,11 @@
 Users = Meteor.users;
+
+Users.helpers({
+  gravatar(size=34, def='mm') {
+    const email = _.first(this.emails);
+    return Gravatar.imageUrl(email.address, {
+      size,
+      default: def,
+    });
+  }
+});
