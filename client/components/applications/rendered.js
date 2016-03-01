e0e08deee90cb2e4eecce87b1454c98b5d90cd6e
Yasar icli <hello@yasaricli.com>
2016-03-01 15:06:11 +0200

    #23 monitoring sparkline interval.

--- client/components/applications/rendered.js
+++ client/components/applications/rendered.js
@@ -4,12 +4,25 @@ Template.applications.onCreated(function() {
 
 Template.application.onRendered(function() {
   const MemorySpark = new Sparkline({
-    height: 100
+    height: 100,
+    tooltipCallback() {
+      return filesize(this.y);
+    }
   });
+
+  this.processIntervalId = Meteor.setInterval(() => {
+    const application = Applications.findOne(this.data._id);
+
+    // ADD APPLICATION
+    MemorySpark.add(application.monit.memory);
+
+    // reload SPARKLINE
+    MemorySpark.reload();
+  }, 10000);
 });
 
-/*
 Template.application.onDestroyed(function() {
+
+  // CLEAR INTERVAL
   Meteor.clearInterval(this.processIntervalId);
 });
-*/
