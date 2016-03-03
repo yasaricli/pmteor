2e6a3ff6d122585aedead7969c7bdb7d627dad44
Yasar icli <hello@yasaricli.com>
2016-03-03 11:25:25 +0200

    #53 application deleted confirm.

--- client/components/applications/events.js
+++ client/components/applications/events.js
@@ -6,10 +6,31 @@ Template.applications.events({
     Meteor.call('stop', this._id);
   },
   'click .delete'() {
-    Meteor.call('delete', this._id);
+    swal({
+      title: TAPi18n.__('are-you-sure-title'),
+      text: TAPi18n.__('are-you-sure-text'),
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#f05050",
+      confirmButtonText: TAPi18n.__('delete-confirm'),
+      closeOnConfirm: false,
+      showLoaderOnConfirm: true,
+      html: false
+    }, () => {
+      Meteor.call('delete', this._id, () => {
+        swal({
+          title: TAPi18n.__('deleted-application-title'),
+          text: TAPi18n.__('deleted-application-text'),
+          type: "success",
+          confirmButtonColor: "#7266ba"
+        });
+      });
+    });
   },
   'click .absoluteUrl'(event) {
     event.preventDefault();
+
+    // in container then  go to click stop.
     window.open(this.absoluteUrl());
   }
 });
