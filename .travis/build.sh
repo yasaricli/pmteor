7441d3539765b38afc73380f41ece25e9e6b1595
Yasar icli <hello@yasaricli.com>
2016-05-03 10:21:11 +0300

    travis rebuild script.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,2 +1,83 @@
-pwd
-ls
+#!/bin/sh
+
+# utilities
+gyp_rebuild_inside_node_modules () {
+  for npmModule in ./*; do
+    cd $npmModule
+
+    isBinaryModule="no"
+    # recursively rebuild npm modules inside node_modules
+    check_for_binary_modules () {
+      if [ -f binding.gyp ]; then
+        isBinaryModule="yes"
+      fi
+
+      if [ $isBinaryModule != "yes" ]; then
+        if [ -d ./node_modules ]; then
+          cd ./node_modules
+          for module in ./*; do
+            cd $module
+            check_for_binary_modules
+            cd ..
+          done
+          cd ../
+        fi
+      fi
+    }
+
+    check_for_binary_modules
+
+    if [ $isBinaryModule = "yes" ]; then
+      echo " > $npmModule: npm install due to binary npm modules"
+      rm -rf node_modules
+      if [ -f binding.gyp ]; then
+        npm install
+        node-gyp rebuild || :
+      else
+        npm install
+      fi
+    fi
+
+    cd ..
+  done
+}
+
+rebuild_binary_npm_modules () {
+  for package in ./*; do
+    if [ -d $package/node_modules ]; then
+      cd $package/node_modules
+        gyp_rebuild_inside_node_modules
+      cd ../../
+    elif [ -d $package/main/node_module ]; then
+      cd $package/node_modules
+        gyp_rebuild_inside_node_modules
+      cd ../../../
+    fi
+  done
+}
+
+# BUILD
+meteor build .
+
+# EXTRACT
+tar -xvf pmteor.tar.gz
+
+# GO TO INSTALL DIR
+cd bundle/programs/server
+
+# NPM REBUILD BINARY
+if [ -d ./npm ]; then
+  cd npm
+  rebuild_binary_npm_modules
+  cd ../
+fi
+
+# PACKAGE INSTALL
+if [ -f package.json ]; then
+  # support for 0.9
+  npm install
+else
+  # support for older versions
+  npm install fibers
+  npm install bcrypt
+fi
